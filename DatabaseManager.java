d35333514dbe0a36e94ae11c28cbbe1c460a8129
Benjamin Goodwin <Benjamin Goodwin>
2018-04-22 20:14:02 -0400

    Workout stuff has been added and minor changes to queries

--- DatabaseManager.java
+++ DatabaseManager.java
@@ -15,6 +15,7 @@ public class DatabaseManager extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "iBenJackedDB";
     private static final int DATABASE_VERSION = 1;
     private static final String TABLE_USERS="Users";
+    private static final String TABLE_WORKOUT ="Profile";
     private static final String USER = "USERNAME";
     private static final String EMAIL = "EMAIL";
     private static final String PASSWORD = "PASSWORD";
@@ -26,9 +27,12 @@ public class DatabaseManager extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         //Creates the database table with the user as the primary key because usernames will
         //be unique therefore they qualify as primary keys
-        String sqlCreateTable = "CREATE TABLE " + TABLE_USERS + " (" +
-                USER + " TEXT PRIMARY KEY, " + EMAIL + " TEXT," + PASSWORD + " TEXT)";
-        sqLiteDatabase.execSQL(sqlCreateTable);
+        String sqlCreateUserTable = "CREATE TABLE " + TABLE_USERS + "(" +
+                USER + " TEXT PRIMARY KEY NOT NULL, " + EMAIL + " TEXT NOT NULL," + PASSWORD + " TEXT NOT NULL);";
+        sqLiteDatabase.execSQL(sqlCreateUserTable);
+        String sqlCreateWorkoutTable = "CREATE TABLE " + TABLE_WORKOUT + "( ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," + USER
+                + "TEXT NOT NULL, WORKOUT STRING NOT NULL, SETS INTEGER NOT NULL, REPS INTEGER NOT NULL, FOREIGN KEY(" + USER + ") REFERENCES " + TABLE_USERS + "(" + USER + "));";
+        sqLiteDatabase.execSQL(sqlCreateWorkoutTable);
     }
 
     @Override
@@ -37,13 +41,32 @@ public class DatabaseManager extends SQLiteOpenHelper {
         onCreate(sqLiteDatabase);
     }
 
+    public void addWorkout(String username, Workout workout){
+        //This will insert the username and the workout into the table workout.
+        //Also, the workouts will be given an auto incrementing id.
+        SQLiteDatabase db = this.getWritableDatabase( );
+        String sqlInsert = "INSERT INTO " + TABLE_WORKOUT + "( ID, " + USER + ", WORKOUT, SETS, REPS)";
+        sqlInsert += " values( null,'" + username +"','"+ workout.getWorkout() + "'," + workout.getSets() + "," + workout.getReps() + ");";
+        db.execSQL( sqlInsert );
+        db.close( );
+    }
+
+    public void removeWorkout(int id){
+        //This will take the id for one of the workouts and delete the workout
+        //associated with that id.
+        SQLiteDatabase db = this.getWritableDatabase();
+        String sqlRemove = "DELETE FROM " + TABLE_WORKOUT + " WHERE ID = " + id + ";";
+        db.execSQL(sqlRemove);
+        db.close();
+    }
+
     public boolean checkUnique(String user){
         //This will check the database to see if the requested username is there.
         //If the cursor counter is 0 then that means that the username is not used.
         //If the cursor counter is greater then 0 then that means that somebody already
         //has that username.
         SQLiteDatabase db = this.getWritableDatabase();
-        String query = "SELECT * FROM " + TABLE_USERS +  " WHERE " + USER  + " = " + user;
+        String query = "SELECT * FROM " + TABLE_USERS +  " WHERE " + USER  + " = " + user + ";";
         Cursor cur = db.rawQuery(query, new String[] { user });
         if (cur.getCount()==0){
             cur.close();
@@ -63,7 +86,7 @@ public class DatabaseManager extends SQLiteOpenHelper {
         //found together. If the cursor is greater than 0 then the username and password
         //are a match.
         SQLiteDatabase db = this.getWritableDatabase();
-        String sql = "SELECT * FROM "+TABLE_USERS+" WHERE user=? AND password=?";
+        String sql = "SELECT * FROM "+TABLE_USERS+" WHERE user=? AND password=?;";
         Cursor cur = db.rawQuery(sql, new String[] { user, password });
         if(cur.getCount()==0){
             cur.close();
@@ -80,8 +103,8 @@ public class DatabaseManager extends SQLiteOpenHelper {
     public void insert(User user) {
         //This will take in a new user and insert them into the database.
         SQLiteDatabase db = this.getWritableDatabase( );
-        String sqlInsert = "INSERT INTO " + TABLE_USERS + "(" + USER + "," + EMAIL + "'" + PASSWORD + ")";
-        sqlInsert += " values("+null+",'"+ user.getName() +"','"+ user.getEmail() + "','" + user.getPassword() + "')";
+        String sqlInsert = "INSERT INTO " + TABLE_USERS + "(" + USER + "," + EMAIL + "," + PASSWORD + ")";
+        sqlInsert += " values("+user.getName() +"','"+ user.getEmail() + "','" + user.getPassword() + "');";
         db.execSQL( sqlInsert );
         db.close( );
     }
@@ -89,7 +112,7 @@ public class DatabaseManager extends SQLiteOpenHelper {
     public void delete(String username){
         //This will take in a username and delete that user from the database.
         SQLiteDatabase db = this.getWritableDatabase();
-        String sqlDelete = "delete from" + TABLE_USERS + "where USERNAME = "+username+"";
+        String sqlDelete = "DELETE FROM " + TABLE_USERS + " WHERE USERNAME = "+username + ";";
         db.execSQL(sqlDelete);
         db.close();
     }
---
+++ Workout.java
@@ -0,0 +1,39 @@
+package com.example.benjamin.databasemanager;
+
+public class Workout {
+
+    private String workout;
+    private int sets;
+    private int reps;
+
+    public Workout(String workoutPassed, int setsPassed, int repsPassed){
+        workout=workoutPassed;
+        sets=setsPassed;
+        reps=repsPassed;
+    }
+
+    public String getWorkout(){
+        return workout;
+    }
+
+    public int getSets(){
+        return sets;
+    }
+
+    public int getReps(){
+        return reps;
+    }
+
+    public void setWorkout(String workoutPassed){
+        workout = workoutPassed;
+    }
+
+    public void setSets(int setsPassed){
+        sets = setsPassed;
+    }
+
+    public void setReps(int repsPassed){
+        reps = repsPassed;
+    }
+
+}
